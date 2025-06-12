cdddb709f86b16dbe44373c6d2ce9d5c2c064fc8
daysprng <daysprng@gmail.com>
2025-06-12 14:26:43 +0300

    feat: api documentation

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Sokrat.im url shortener API"', () => {
+      expect(appController.getHello()).toBe('Sokrat.im url shortener API');
     });
   });
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,16 @@ async function bootstrap() {
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Sokrat.im url shortener API')
+    .setDescription('REST API for URL Shortener')
+    .setVersion('1.0')
+    .addTag('url', 'URL Shortener operations')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 4001);
 }
